e50ac1624299e53901362affdbd1bdcb964f1273
Teodora <roksanaim@gmail.com>
2024-03-21 15:13:10 +0200

    css page log in

--- WebAPI/src/app/app.module.ts
+++ WebAPI/src/app/app.module.ts
@@ -1,21 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {
+  SocialLoginModule,
+} from '@abacritt/angularx-social-login';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    LogInComponent,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- WebAPI/src/app/components/log-in/log-in.component.ts
+++ WebAPI/src/app/components/log-in/log-in.component.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {NgIf} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { UserLogIn } from '../../models/userLogIn';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (
+      // control.dirty || control.touched || 
+      isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule, 
+    MatInputModule, 
+    ReactiveFormsModule, 
+    NgIf, 
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class LogInComponent {
   userLog = new UserLogIn;
+  hidePassword = true;
+  hideRequired="true";
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 
   constructor(private router: Router) { }
 
